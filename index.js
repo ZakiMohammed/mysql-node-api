044bc97f8750d89191f3b93913808f18f4bc7d94
Zaki Shaikh <zaki-shaikh@outlook.com>
2020-12-22 00:03:03 +0530

    CORS pacakge added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
 const port = 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
